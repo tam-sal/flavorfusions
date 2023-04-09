d2385e091a94eeefe4b0d556d0eee213c85fa0c7
tam-sal <t.saleh.gc@gmail.com>
2023-04-09 20:40:38 -0300

    Ready to deploy

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import { Provider } from 'react-redux'
 import store from '../src/Redux/store/store'
 import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
-
+//* deployed
 if (process.env.NODE_ENV === 'production') disableReactDevTools()
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
